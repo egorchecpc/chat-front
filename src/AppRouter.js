3c725111bf03baf6ac95c421b0ea8451eeddc9af
egorchecpc <egorchecpc@gmail.com>
2025-07-15 16:42:09 +0300

    fix bug with gh pages

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,10 @@
-// src/AppRouter.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import UploadPage from "./UploadPage";
 
 function AppRouter() {
   return (
-    <Router>
+    <Router basename="/chat-front">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/upload" element={<UploadPage />} />
